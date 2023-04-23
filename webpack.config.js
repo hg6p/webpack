32e4de1fde784c90986f81d36257c9978eb7c05f
hg6p <ivernjekurac@gmail.com>
2023-04-23 20:50:10 +0200

    add extract css and purge unused css

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
-const { mode } = require("webpack-nano/argv");
-const { merge } = require("webpack-merge");
-const parts = require("./webpack.parts");
+const { mode } = require('webpack-nano/argv');
+const { merge } = require('webpack-merge');
+const parts = require('./webpack.parts');
+
+const cssLoaders = [parts.tailwind()];
 
 const commonConfig = merge([
-  { entry: ["./src"] },
-  parts.page({ title: "Demo" }),
-  parts.loadCss(),
+  { entry: ['./src'] },
+  parts.page({ title: 'Demo' }),
+  parts.extractCSS({ loaders: cssLoaders }),
 ]);
-const productionConfig = merge([]);
+
+const productionConfig = merge([parts.eliminateUnusedCSS()]);
 
 const developmentConfig = merge([
-  { entry: ["webpack-plugin-serve/client"] },
+  { entry: ['webpack-plugin-serve/client'] },
   parts.devServer(),
 ]);
 
 const getConfig = (mode) => {
   switch (mode) {
-    case "production":
+    case 'production':
       return merge(commonConfig, productionConfig, { mode });
-    case "development":
+    case 'development':
       return merge(commonConfig, developmentConfig, { mode });
     default:
       throw new Error(`Trying to use an unknown mode, ${mode}`);
--- webpack.parts.js
+++ webpack.parts.js
@@ -1,12 +1,18 @@
-const { WebpackPluginServe } = require("webpack-plugin-serve");
-const { MiniHtmlWebpackPlugin } = require("mini-html-webpack-plugin");
+const { WebpackPluginServe } = require('webpack-plugin-serve');
+const { MiniHtmlWebpackPlugin } = require('mini-html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { PurgeCSSPlugin } = require('purgecss-webpack-plugin');
+const glob = require('glob');
+const path = require('path');
+
+const ALL_FILES = glob.sync(path.join(__dirname, 'src/*.js'));
 
 exports.devServer = () => ({
   watch: true,
   plugins: [
     new WebpackPluginServe({
       port: parseInt(process.env.PORT, 10) || 8080,
-      static: "./dist", // Expose if output.path changes
+      static: './dist', // Expose if output.path changes
       liveReload: true,
       waitForBuild: true,
     }),
@@ -17,13 +23,59 @@ exports.page = ({ title }) => ({
   plugins: [new MiniHtmlWebpackPlugin({ context: { title } })],
 });
 
+//unused
 exports.loadCss = () => ({
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
+});
+
+exports.extractCSS = ({ options = {}, loaders = [] } = {}) => {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            { loader: MiniCssExtractPlugin.loader, options },
+            'css-loader',
+          ].concat(loaders),
+          sideEffects: true,
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+    ],
+  };
+};
+exports.tailwind = () => ({
+  loader: 'postcss-loader',
+  options: {
+    postcssOptions: {
+      plugins: [require('tailwindcss')()],
+    },
+  },
+});
+
+exports.eliminateUnusedCSS = () => ({
+  plugins: [
+    new PurgeCSSPlugin({
+      paths: ALL_FILES, // Consider extracting as a parameter
+      extractors: [
+        {
+          extractor: (content) =>
+            content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
+          extensions: ['html'],
+        },
+      ],
+    }),
+  ],
 });
